9a691a65139df9b27aa16ec34cc0bebe32422ada
David <David.Lange@cern.ch>
2019-08-01 20:42:50 +0200

    idea for py3 py2 version separation

--- pip/cmsdist_packages.py
+++ pip/cmsdist_packages.py
@@ -7,5 +7,14 @@ def packages(virtual_packages):
     if line.startswith('#'):continue
     if not '==' in line: continue
     (pkg, ver) = line.strip().split('==',1)
-    virtual_packages['py2-'+pkg]='%s/package.sh "py2-%s" "%s"' % (pkg_dir, pkg, ver)
+    verSP=ver.split(',')
+    ver2=verSP[0]
+    if len(verSP)>1:
+      ver3=verSP[1]
+    else:
+      ver3=ver2
+    if ver2 != 'None':
+      virtual_packages['py2-'+pkg]='%s/package.sh "py2-%s" "%s" "%s"' % (pkg_dir, pkg, ver2, 'py2')
+    if ver3 != 'None':
+      virtual_packages['py3-'+pkg]='%s/package.sh "py3-%s" "%s" "%s"' % (pkg_dir, pkg, ver3, 'py3')
   return
--- pip/package.sh
+++ pip/package.sh
@@ -4,6 +4,13 @@ if [ -e "${dir}/$1.spec" ] ; then
   cat "${dir}/$1.spec"
 else
   echo "### RPM external $1 $2"
+  if [ "$4" == "py2" ] ; then
+    echo "%define doPython3 no"
+  fi
+  if [ "$4" == "py3" ] ; then
+    echo "%define doPython2 no"
+  fi
+
   echo "## IMPORT build-with-pip"
   if [ -f "${dir}/$1.file" ] ; then
     subdir=$(basename $dir)
